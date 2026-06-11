abc0e533ae9ac7236876c4b23a235168964eed99
Tushar <tushar.kadam@auxilo.com>
2026-06-11 11:23:47 +0530

    Designed the file parsing service - Added additional configurations and limiters in config.py - Validated file path and file extension - Converted csv data into dict - Returned converted data

--- config.py
+++ config.py
@@ -10,3 +10,17 @@ class Config:
     DB_NAME = os.getenv('DB_NAME')
 
     GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+
+    UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', 'uploads')
+    ALLOWED_EXTENSION = 'csv'
+
+    REQUIRED_COLUMNS = {
+        'tx_id',
+        'date',
+        'merchant',
+        'amount',
+        'currency',
+        'status',
+        'category',
+        'account_id'
+    }
--- services/file_parsing_service.py
+++ services/file_parsing_service.py
@@ -1,1 +1,31 @@
-from database.db import get_db_connection
+import pandas as pd
+from config import Config
+import logging
+logging.basicConfig(
+    level = logging.INFO,
+    format = "%(asctime)s [%(levelname)s] %(message)s"
+)
+logger = logging.getLogger()
+
+def validate_csv(filename,filepath):
+    if filename is None:
+        logger.error('Filename not provided')
+        raise ValueError('filename must not be None')
+    
+    if filepath is None:
+        logger.error('File path is not provided')
+        raise ValueError('filepath must not be None')
+
+    stripped_extension = filename.strip().split('.')
+    extension = stripped_extension[1]
+    if extension != Config.ALLOWED_EXTENSION:
+        logger.warning(f'Given file is not a csv file. Received file_extension:{extension}')
+        return None
+    df = pd.read_csv(filepath)
+    csv_data = df.to_dict(orient='records')
+
+    if not csv_data:
+        logger.warning('Csv is empty or data not parsed')
+        return None
+    
+    return csv_data
